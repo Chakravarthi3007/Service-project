76c3bf528f318c8bc2b16e4e42d2ffff0fb2a045
chakravarthi3007 <chakravarthi3007@gmail.com>
2020-07-30 01:40:00 +1000

    Sorting & Table View

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { DataService } from '../Services/data.service';
 
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -16,8 +17,28 @@ export class ListComponent implements OnInit {
   ngOnInit(): void {
     this._dataService.getCountryNames().subscribe((res) => {
       this.countryList = res['result'];
+      this.countryList.sort(this.compare);
+      console.log(this.countryList);
+    });
+  }
+  compare(a, b) {
+    const bandA = a.name.toUpperCase();
+    const bandB = b.name.toUpperCase();
+
+    let comparison = 0;
+    if (bandA > bandB) {
+      comparison = 1;
     }
-    );
+    else if (bandA < bandB) {
+      comparison = -1;
+    }
+    return comparison;
+  }
+  toggle = false;
+
+  onClick(event) {
+    console.log(event);
+    this.toggle = !this.toggle;
   }
 
 }
